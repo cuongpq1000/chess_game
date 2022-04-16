227c1e277e208475cd1c3c4d148e619c83c13d20
Luis Vazquez Morales <lvazquez@vt.edu>
2022-04-15 23:01:20 -0400

    chessboard added

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,8 +2,9 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from './login/login.component';
 import { RegisterComponent } from './register/register.component';
+import {GameComponent} from "./game/game.component";
 
-const routes: Routes = [{path: 'login', component: LoginComponent}, {path: 'register', component: RegisterComponent}];
+const routes: Routes = [{path: 'login', component: LoginComponent}, {path: 'game', component: GameComponent}, {path: 'register', component: RegisterComponent}];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,21 +9,31 @@ import { RegisterComponent } from './register/register.component';
 import {MatCardModule} from '@angular/material/card';
 import {MatFormFieldModule} from '@angular/material/form-field'
 import {MatIconModule} from '@angular/material/icon'
+import {MatGridListModule} from "@angular/material/grid-list";
+import {MatListModule} from "@angular/material/list";
+import {MatToolbarModule} from "@angular/material/toolbar";
+import { GameComponent } from './game/game.component';
+import {DragDropModule} from '@angular/cdk/drag-drop';
+
 @NgModule({
   declarations: [
     AppComponent,
     LoginComponent,
-    RegisterComponent
+    RegisterComponent,
+    GameComponent
   ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule,
-    BrowserAnimationsModule,
-    MatCardModule,
-    MatFormFieldModule,
-    MatIconModule
+    imports: [
+        BrowserModule,
+        AppRoutingModule,
+        BrowserAnimationsModule,
+        MatCardModule,
+        MatFormFieldModule,
+        MatIconModule,
+        MatGridListModule,
+        MatListModule,
+        MatToolbarModule
 
-  ],
+    ],
   providers: [],
   bootstrap: [AppComponent]
 })
